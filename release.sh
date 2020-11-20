06a5c7425703844b7b8cad497cb2cbd0bb0e5254
rajicdalibor <rajicdalibor@yahoo.com>
2020-11-20 16:16:29 +0100

    added release jobs

--- release.sh
+++ release.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-# release the next patch if CIRCLE_BRANCH is release branch
 
-set -e
+set -eo
 
+if [[ "${CIRCLE_BRANCH}" =~ "master" ]]; then
+  VERSION=$(npm run version --prefix client --silent)
 
-VERSION=$(npm run version --prefix client --silent)
+  echo "Configure git"
+  git config user.email "rajicdalibor@yahoo.com"
+  git config user.name "rajicdalibor"
 
-echo "Configurate git"
-git config user.email "rajicdalibor@yahoo.com"
-git config user.name "rajicdalibor"
-echo "Git commit release '${VERSION}'"
-git add client/package.json client/package-lock.json
-git commit -m "[skip ci] release '${VERSION}'"
-git tag "${VERSION}"
-git push --set-upstream origin "$CIRCLE_BRANCH"
-git push origin --tags
-npm run version --prefix client
-echo "Releasing"
+  echo "Git commit release '${VERSION}"
+  git add client/package.json client/package-lock.json
+  git commit -m "[skip ci] release '${VERSION}'"
+  git tag ${VERSION}
+
+  git push --set-upstream origin $CIRCLE_BRANCH
+  git push origin --tags
+else
+  echo "Skip releasing"
+fi
